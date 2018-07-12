f1ef0029e40319418c05d0099adf5c8558d2b872
gaoliye <soargly@163.com>
2018-07-12 16:02:44 +0800

    优化轮播图组件

--- src/components/LoopImg/src/LoopImg.vue
+++ src/components/LoopImg/src/LoopImg.vue
@@ -5,9 +5,9 @@
                 'transform': 'translate('+ distance +'px, 0)',
                 'transition-duration': transTime +'s'
             }"
-            @touchstart="touchstart($event)"
-            @touchmove.prevent="touchmove($event)"
-            @touchend="touchend($event)"
+            @touchstart.stop="touchstart($event)"
+            @touchmove.prevent.stop="touchmove($event)"
+            @touchend.stop="touchend($event)"
             ref="wrap">
             <li v-for="(item, index) in innerImgs" :key="index" class="snow-loop-item">
                 <a :href="item.link" class="link">
@@ -43,7 +43,7 @@
             return {
                 wrap: null,
                 timer: 0,// 定时器
-                index: 1,// 图片索引
+                index: 0,// 图片索引
                 count: 0,// 图片的数量
                 stepLen: 0,// 每次滑动的距离
                 isTouching: false,// 手指正在触碰
@@ -69,6 +69,7 @@
                     }
                 });
                 if (this.imgs.length > 1) {
+                    this.index = 1;
                     arr.push(arr[0]);
                     arr.unshift(arr[arr.length - 2]);
                 }
@@ -82,23 +83,34 @@
                 return -this.index * this.stepLen;
             }
         },
+        watch: {
+            index(val) {
+                if (0 < val && val < this.count - 1) {
+                    this.$emit('change', val - 1);
+                }
+            },
+            imgs() {
+                this.active();
+            }
+        },
         mounted() {
+            // 一张图片时什么都不做；
             this.wrap = this.$refs.wrap;
             this.stepLen = this.wrap.offsetWidth;
             window.addEventListener('resize', () => { this.stepLen = this.wrap.offsetWidth; }, false);
             this.$nextTick(() => {
                 this.transitionEnd();
+                if (this.count <= 1) return;
                 this.active();
             });
         },
         methods: {
             active() {
-                if (this.count > 1) {// 判断当前图片的数量大于1轮播
-                    this.timer = setInterval(() => {
-                        this.index ++;
-                        this.transTime = this.duration;// 接受动画过渡时间
-                    }, (this.gapTime + this.duration) * 1000);
-                }
+                clearInterval(this.timer);
+                this.timer = setInterval(() => {
+                    this.index ++;
+                    this.transTime = this.duration;// 接受动画过渡时间
+                }, (this.gapTime + this.duration) * 1000);
             },
             transitionEnd() {// 图片切换结束之后处理
                 // transitionend 的兼容性
@@ -117,27 +129,27 @@
                 });
             },
             touchstart(e) {// 开始滑动
-                if(!this.swipeable) return;
+                if(!this.swipeable || this.count <= 1) return;
                 // 已经记录滑动的距离清零
                 this.deltaX = 0;
                 this.isTouching = true;
-                clearInterval(this.timer);
                 this.transTime = 0;
                 let point = this.getPoint(e);
                 // 记录指尖触碰的初始位置
                 this.startX = point.pageX;
                 // 为避免队列尾部空白，端点处理
+                clearInterval(this.timer);
                 this.endPoint();
             },
             touchmove(e) {// 滑动过程
-                if(!this.swipeable) return;
+                if(!this.swipeable || this.count <= 1) return;
                 // 是否是触碰滑动中
                 let point = this.getPoint(e);
                 // 获取手指滑动的间距
                 this.deltaX = point.pageX - this.startX;
             },
             touchend(e) {// 滑动结束
-                if(!this.swipeable) return;
+                if(!this.swipeable || this.count <= 1) return;
                 // 手指离开
                 this.isTouching = false;
                 this.transTime = 0.3;
@@ -146,11 +158,10 @@
                         this.index--;
                     }
                 }else {
-                     if (Math.abs(this.deltaX / this.stepLen) > 0.3) {
+                    if (Math.abs(this.deltaX / this.stepLen) > 0.3) {
                         this.index ++;
                     }   
                 }
-                // 清零 deltaX;
                 this.active();
             },
             getPoint(e) {// 默认以第一个手指的位置计算
@@ -172,6 +183,7 @@
 <style lang='scss' scoped>
     .snow-loop-img-container {
         position: relative;
+        height: 100%;
         overflow: hidden;
         .snow-loop-img-wrap {
             display: box;
